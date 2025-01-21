27bfb78cd8fdc8ffbfc1a6d6d3f07c1f28c1a361
daniel costa <dalpcosta@gmail.com>
2025-01-21 16:20:44 -0300

    pronto.

--- logic8.java
+++ logic8.java
@@ -1,11 +1,27 @@
 public class logic8{
     public static void main(String[] args) {
         int i, j, n = 5;
+        
         for(i = n / 2; i <= n; i+=2){
             for(j = 1;j < n - i; j+=2){
                 System.out.print(" ");
             }
-            
+            for(j = 1;j <= i;j++){
+                System.out.print("*");
+            }
+            for(j = 1;j <= n - i;j++){
+                System.out.print(" ");
+            }
+            System.out.println(" ");
+        }
+        for(i = n; i >= 1; i--){
+            for (j = n - i; j > 0; j--){
+                System.out.print(" ");
+            }
+            for(j = 1;j <= (i * 2) - 1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
         }
     }
 }
